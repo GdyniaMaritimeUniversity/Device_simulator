bf169ab7e5eb6f57d4475d725320aca175342248
piotrdzik127@gmail.com <Sniggers@4>
2025-06-04 22:34:05 +0200

    Token added

--- output.py
+++ output.py
@@ -1,8 +1,12 @@
 import requests
 
+headers = {
+    "Authorization": "Bearer 5R3661N"
+}
+
 def send_to_api(data, url):
     try:
-        response = requests.post(url, json=data)
+        response = requests.post(url, json=data, headers=headers)
         if response.status_code == 200:
             print("✔️  Data sent successfully.")
         else:
